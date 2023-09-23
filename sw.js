668e821bcdb58ccb2f9dd17481097377f0f0d455
hyms0209 <42196927+hyms0209@users.noreply.github.com>
2023-09-23 16:55:56 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,20 @@
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('my-cache').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.html'
+        // 필요한 다른 리소스들도 추가
+      ]);
+    })
+  );
+});
+
 self.addEventListener('fetch', function(event) {
   event.respondWith(
-      caches.match(event.request).then(function(response) {
-          return response || fetch(event.request);
-      })
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
   );
 });
